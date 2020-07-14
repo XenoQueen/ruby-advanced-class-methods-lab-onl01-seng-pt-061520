ff2554df9cc66c3f27ed63960c11e2634737d83b
Maika Kailani <queenmab051@gmail.com>
2020-07-14 08:22:00 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,29 +10,29 @@ class Song
     self.class.all << self
   end
   
-  def Song.create
+  def self.create
     song = Song.new
     song.save
     song
   end
   
-  def Song.new_by_name(name)
+  def self.new_by_name(name)
     song = self.new
     song.name = name
     song
   end
   
-  def Song.create_by_name(name)
+  def self.create_by_name(name)
     song = self.new_by_name(name)
     song.save
     song
   end
   
-  def Song.find_by_name(name)
+  def self.find_by_name(name)
     @@all.find {|song| song.name == name}
   end
   
-  def song.find_or_create_by_name(name)
+  def self.find_or_create_by_name(name)
     if self.find_by_name(name)
       self.find_by_name(name)
     else
@@ -40,11 +40,11 @@ class Song
     end
   end
   
-  def song.alphabetical
+  def self.alphabetical
     @@all.sort_by {|song| song.name}
   end
   
-  def song.new_from_filename(filename)
+  def self.new_from_filename(filename)
     artist_and_song = filename.split(".").first
     artist_and_song = artist_and_song.split(" - ")
     artist = artist_and_song[0]
@@ -55,11 +55,11 @@ class Song
     new_song
   end
   
-  def song.create_from_filename(filename)
+  def self.create_from_filename(filename)
     self.new_from_filename(filename).save
   end
   
-  def song.destroy_all
+  def self.destroy_all
     @@all = []
   end
 end
